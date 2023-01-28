51cb51c41a6ec3329d276f9072008e577b020757
Q1unt, Дмитрий <dima.kunitsien.03@gmail.com>
2023-01-28 08:53:08 +0300

    final_draft

--- project.py
+++ project.py
@@ -14,7 +14,7 @@ with open('files.txt', 'rt', encoding='utf-8') as file:
             })
         file.readline()
         cook_book[dish_name] = _list
-# print(cook_book)
+print(cook_book)
 
 
 def get_shop_list_by_dishes(dishes, person_count):
@@ -27,43 +27,38 @@ def get_shop_list_by_dishes(dishes, person_count):
         food = key['ingredient_name']
         vocabulary1 = {'quantity': int(key['quantity']) * person_count,
                'measure': key['measure']}
-        if food not in vocabulary:
-            vocabulary[food] = vocabulary1
-        else:
-            vocabulary[food] = upvocabulary1
+        vocabulary[food] = vocabulary1
+
     print(vocabulary)
 
 
-(get_shop_list_by_dishes(['Фахитос', 'Омлет'], 2))
+(get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2))
 
-# with open('1.txt', 'r', encoding= 'utf - 8') as f:
-#     vocabulary1 = {}
-#     res = len(f.readlines())
-#     vocabulary1['1.txt'] = res
-#
-# with open('2.txt', 'r', encoding= 'utf - 8') as f:
-#     res = len(f.readlines())
-#     vocabulary1['2.txt'] = res
-#
-# with open('3.txt', 'r', encoding= 'utf - 8') as f:
-#     res = len(f.readlines())
-#     vocabulary1['3.txt'] = res
-#
-# sorted_values = sorted(vocabulary1.values())
-# sorted_dict = {}
-# for i in sorted_values:
-#     for k in vocabulary1.keys():
-#         if vocabulary1[k] == i:
-#             sorted_dict[k] = vocabulary1[k]
-#
-# for key in sorted_dict:
-#     with open(key, 'r',encoding= 'utf - 8') as f:
-#         with open(f'4.txt', 'a', encoding= 'utf - 8') as file:
-#             file.write(key + '\n')
-#             file.write(str(sorted_dict[key]))
-#             file.write('\n')
-#             file.write(f.read() + '\n')
-#             print(file)
+with open('1.txt', 'r', encoding= 'utf - 8') as f:
+    vocabulary1 = {}
+    res = len(f.readlines())
+    vocabulary1['1.txt'] = res
 
+with open('2.txt', 'r', encoding= 'utf - 8') as f:
+    res = len(f.readlines())
+    vocabulary1['2.txt'] = res
 
+with open('3.txt', 'r', encoding= 'utf - 8') as f:
+    res = len(f.readlines())
+    vocabulary1['3.txt'] = res
 
+sorted_values = sorted(vocabulary1.values())
+sorted_dict = {}
+for i in sorted_values:
+    for k in vocabulary1.keys():
+        if vocabulary1[k] == i:
+            sorted_dict[k] = vocabulary1[k]
+
+for key in sorted_dict:
+    with open(key, 'r',encoding= 'utf - 8') as f:
+        with open(f'4.txt', 'a', encoding= 'utf - 8') as file:
+            file.write(key + '\n')
+            file.write(str(sorted_dict[key]))
+            file.write('\n')
+            file.write(f.read() + '\n')
+            print(file)
